76ec9eabed9e4d6b6d239ad07228818e9910ca5c
Per1vai <felipelinchiu321@gmail.com>
2020-11-27 18:21:38 -0600

    se pudo hacer la mayoria solo que no funciona con algunas llaves

--- P3Lab7_FelipeLin/main.cpp
+++ P3Lab7_FelipeLin/main.cpp
@@ -19,8 +19,9 @@
 
 using namespace std;
 
-string cifradocesar_adelante(string, int, int, int);
-string cifradocesar_atras(string, int, int, int);
+char cifradocesar_adelante(char, int);
+char cifradocesar_atras(char, int);
+string recursivo(string, int,int);
 
 int main(int argc, char** argv) {
     vector<Persona*> lista_persona;
@@ -45,8 +46,8 @@ int main(int argc, char** argv) {
                 cout << "Ingrese su contraseña:" << endl;
                 cin >> pass;
 
-                llave = rand() % 1 + 15;
-
+                llave = rand() % 4 + 1;
+                
                 Persona* p = new Persona(nombre, apellido, pass, llave);
 
                 lista_persona.push_back(p);
@@ -64,11 +65,12 @@ int main(int argc, char** argv) {
 
                 cout << "Ingrese su contraseña:" << endl;
                 cin >> login_contra;
-
+                int persona_selec;
 
                 for (int i = 0; i < lista_persona.size(); i++) {
 
                     if (lista_persona.at(i)->getNombre() == login_name && lista_persona.at(i)->getPass() == login_contra) {
+                        persona_selec = i;
                         int menu;
                         cout << "1. Enviar mensaje\n2. Ver mensaje\n3. Ver mi llave\n4. Salir\n";
                         cout << "Ingrese la opcion: ";
@@ -77,6 +79,51 @@ int main(int argc, char** argv) {
                             switch (menu) {
                                 case 1:
                                 {
+                                    int persona_enviar;
+
+                                    cout << "*ENVIAR MENSAJE*" << endl;
+
+                                    for (int i = 0; i < lista_persona.size(); i++) {
+                                        cout << i << "." << lista_persona[persona_selec]->getNombre() << endl;
+                                    }
+
+                                    cout << "Ingrese el numero de la persona que quiere enviar: " << endl;
+                                    cin >> persona_enviar;
+
+                                    string mensaje;
+                                    cout << "Ingrese el mensaje que quiere codificar: " << endl;
+                                    cin >> mensaje;
+
+                                    string mensaje_codificado;
+                                    
+                                    cout << "llave : " << lista_persona[persona_selec]->getLlave() << endl; 
+                                    mensaje_codificado = recursivo(mensaje, lista_persona[persona_selec]->getLlave(),0);
+
+                                    lista_persona[persona_enviar]->addMensaje(mensaje_codificado);
+
+
+
+                                    break;
+                                }
+                                case 2:
+                                {
+                                    cout << "*VER MENSAJES*" << endl;
+
+                                    for (int i = 0; i < lista_persona.size(); i++) {
+                                        cout << i + 1 << ". De: " << lista_persona[persona_selec]->getNombre() << " ";
+                                        for (int i = 0; i < lista_persona[persona_selec]->getLista().size(); i++) {
+                                            cout << lista_persona[persona_selec]->getLista().at(i);
+                                        }
+                                        cout << endl;
+                                    }
+
+                                    break;
+                                }
+                                case 3:
+                                {
+                                    cout << "*VER MI LLAVE*" << endl;
+
+                                    cout << lista_persona[persona_selec]->getNombre() << " su llave es " << lista_persona[persona_selec]->getLlave() << endl;
 
                                     break;
                                 }
@@ -86,6 +133,9 @@ int main(int argc, char** argv) {
                                     break;
                                 }
                             }
+                            cout << "1. Enviar mensaje\n2. Ver mensaje\n3. Ver mi llave\n4. Salir\n";
+                            cout << "Ingrese la opcion: ";
+                            cin >> menu;
                         }
                     } else {
                         cout << "Ingreso mal el nombre o la contraseña" << endl;
@@ -108,26 +158,57 @@ int main(int argc, char** argv) {
     }
 
 
-
+    
 
 }
 
-string cifradocesar_adelante(string texto, int desplazamiento, int tamanio, int aux) {
-    for (int i = 0; i < tamanio; i++) {
-        aux = texto[i];
-        aux = aux + desplazamiento;
-        texto[i] = aux;
+string recursivo(string msj, int llave,int contador_llave) {
+    
+    bool cambio = false;
+    if (llave == 0) {
+        return msj;
+    } else {
+        for (int i = 0; i < msj.size(); i+=llave) {
+
+            if (!cambio) {
+                for (int j = 0; j < llave; j++) {
+                    msj[contador_llave] = cifradocesar_adelante(msj[contador_llave], llave);
+                    contador_llave++;
+                }
+                cambio = true;
+
+
+            } else {
+                for (int j = 0; j < llave; j++) {
+                    msj[contador_llave] = cifradocesar_atras(msj[contador_llave], llave);
+                    contador_llave++;
+                }
+
+                cambio = false;
+            }
+
+        }
+
+        recursivo(msj, llave - 1,contador_llave);
     }
+    return msj;
+}
+
+char cifradocesar_adelante(char texto, int desplazamiento) {
+    int aux;
+    aux = texto;
+    aux = aux + desplazamiento;
+    texto = aux;
 
     return texto;
+
 }
 
-string cifradocesar_atras(string texto, int desplazamiento, int tamanio, int aux) {
-    for (int i = 0; i < tamanio; i++) {
-        aux = texto[i];
-        aux = aux - desplazamiento;
-        texto[i] = aux;
-    }
+char cifradocesar_atras(char texto, int desplazamiento) {
+    int aux;
+    aux = texto;
+    aux = aux - desplazamiento;
+    texto = aux;
 
     return texto;
 
